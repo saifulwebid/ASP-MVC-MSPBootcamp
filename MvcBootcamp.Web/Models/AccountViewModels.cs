3211dea4b7e598efe71daff8b86af4d727abd154
Muhammad Saiful Islam <muhammad@saiful.web.id>
2016-05-07 09:50:07 +0700

    Login dengan username (bukan email)

--- MvcBootcamp.Web/Models/AccountViewModels.cs
+++ MvcBootcamp.Web/Models/AccountViewModels.cs
@@ -49,9 +49,13 @@ namespace MvcBootcamp.Web.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        //[Required]
+        //[Display(Name = "Email")]
+        //[EmailAddress]
+        //public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -64,6 +68,10 @@ namespace MvcBootcamp.Web.Models
 
     public class RegisterViewModel
     {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
