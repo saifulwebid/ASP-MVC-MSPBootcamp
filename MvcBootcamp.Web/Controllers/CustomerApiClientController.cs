9e7fa14c9a680b61425763b9516dd5d09b494470
Muhammad Saiful Islam <muhammad@saiful.web.id>
2016-05-07 14:52:55 +0700

    Buat controller baru untuk consume WebAPI

---
+++ MvcBootcamp.Web/Controllers/CustomerApiClientController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBootcamp.Web.Controllers
+{
+    public class CustomerApiClientController : Controller
+    {
+        // GET: CustomerApiClient
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
