cf721a7f7682e0edd2c15e0ae62a0d73edd28d83
Muhammad Saiful Islam <muhammad@saiful.web.id>
2016-05-08 09:42:44 +0700

    Buat CustomerMockTest

---
+++ MvcBootcamp.Web.Tests/CustomerMockTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using MvcBootcamp.NorthwindRepository.Interfaces;
+using MvcBootcamp.NorthwindRepository.Repositories;
+using MvcBootcamp.DAL;
+using Telerik.JustMock;
+using MvcBootcamp.Web.Controllers;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Linq;
+
+namespace MvcBootcamp.Web.Tests
+{
+    [TestClass]
+    public class CustomerMockTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
