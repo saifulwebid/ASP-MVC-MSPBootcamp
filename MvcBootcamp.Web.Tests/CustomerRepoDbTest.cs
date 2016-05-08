231653fff4cf55179b7afb613368f03acc5f161d
Muhammad Saiful Islam <muhammad@saiful.web.id>
2016-05-08 10:29:42 +0700

    Tambah testing untuk Repository Customer

---
+++ MvcBootcamp.Web.Tests/CustomerRepoDbTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using MvcBootcamp.DAL;
+using MvcBootcamp.NorthwindRepository.Repositories;
+using System.Linq;
+
+namespace MvcBootcamp.Web.Tests
+{
+    [TestClass]
+    public class CustomerRepoDbTest
+    {
+        [TestMethod]
+        public void GetAllData_ReturnAllCustomers()
+        {
+            var repo = new CustomerRepository();
+            var allCust = repo.GetAllData().ToList();
+            Assert.AreEqual(91, allCust.Count());
+        }
+
+        [TestMethod]
+        public void Search_ReturnACustomer()
+        {
+            var repo = new CustomerRepository();
+            var cust = repo.Search("ALFKI");
+            Assert.AreNotEqual("Alfred F", cust.CompanyName);
+        }
+    }
+}
